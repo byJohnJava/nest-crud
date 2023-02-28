9db86f15280f2bcdfc281d83bcd52d36c1786f64
João Santos <j.joaosantos@outlook.com>
2023-02-28 14:27:55 -0300

    feat: installed and configured prisma

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  // quando o módulo for inicializado, o método onModuleInit() será chamado e a conexão com o banco de dados será estabelecida
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
